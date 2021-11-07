b9654442d606e2548123cc4c6c3f59dc4ff2814c
tognia <h.tognia@ziyouma-agency.com>
2021-11-07 19:01:24 +0100

    Unit tests with React Testing Library 07112021

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-export const parameters = {
+/*export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
@@ -6,4 +6,12 @@ export const parameters = {
       date: /Date$/,
     },
   },
+};*/
+
+
+import '../src/index.css';
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
 };
